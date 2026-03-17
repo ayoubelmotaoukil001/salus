2aebfeeffb4e9fc6fb27ae822f62f250cf22b5fb
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-03-17 09:17:24 +0000

    feat: setup Symptom model

--- app/Models/Symptom.php
+++ app/Models/Symptom.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Models;
+use App\Models\User ;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Symptom extends Model
 {
-    
+    protected $fillable = ['user_id', 'name', 'severity', 'description', 'date_recorded', 'notes'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

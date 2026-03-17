9499bf16d7d9feb0bc116275c6c214f01db12e3f
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-03-17 21:37:44 +0000

    migration: create doctors table with correct field names

--- database/migrations/2026_03_16_212751_create_doctors_table.php
+++ database/migrations/2026_03_16_212751_create_doctors_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name') ;
+            $table->string("specialty") ;
+            $table ->string('city') ;
+            $table->integer('years_of_experience') ;
+            $table->decimal('consultation_price') ;
+            $table->json('available_days') ;
         });
     }
 

128e770c9387786e4b3258cca1d0f9ab8ce80478
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-03-17 21:23:11 +0000

    feat: setup HealthAdvice model with user relationship

--- app/Models/HealthAdvice.php
+++ app/Models/HealthAdvice.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class HealthAdvice extends Model
 {
-    //
+    protected $fillable = ['user_id', 'advice', 'symptoms_used', 'generated_at'];
+    public function user()
+    {
+       return $this->belongsTo(User::class);
+    }
 }

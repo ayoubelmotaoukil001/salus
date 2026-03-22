7683b5d3750b36a3cfa7a3c779ff247919dfbaf4
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-03-22 04:35:32 +0100

    add symptom resource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\SymptomController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -9,4 +11,9 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+
+    Route::get('/doctors', [DoctorController::class, 'index']);
+    Route::get('/doctors/{doctor}', [DoctorController::class, 'show']);
+
+    Route::apiResource('symptoms', SymptomController::class);
 });

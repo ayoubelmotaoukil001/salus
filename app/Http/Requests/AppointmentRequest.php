2798be4eafcf98b0d1389860395b5f5d70686d3c
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-03-28 14:59:48 +0100

    feat(AppointmentRequest.php) edit add validation rules with future date constraint

---
+++ app/Http/Requests/AppointmentRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AppointmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'doctor_id'        => ['required', 'exists:doctors,id'],
+            'appointment_date' => ['required', 'date'],
+            'notes'            => ['nullable', 'string'],
+        ];
+    }
+}

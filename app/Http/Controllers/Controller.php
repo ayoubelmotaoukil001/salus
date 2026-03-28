e6cd566db516a4cd14ac195e971e7763e95dfb9b
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-03-28 14:55:10 +0100

    feat(Controller.php) edit add global Swagger Info and Server attributes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA; // ركز هنا.. غيرنا Annotations لـ Attributes
 
-use App\Traits\ApiResponse;
-
+#[OA\Info(version: "1.0.0", title: "Salus AI API", description: "Documentation de l'API Salus")]
+#[OA\Server(url: "http://localhost:8000", description: "Serveur Local")]
 abstract class Controller
 {
-    use ApiResponse;
+    public function success($data, $message = 'Success', $status = 200)
+    {
+        return response()->json(['success' => true, 'data' => $data, 'message' => $message], $status);
+    }
+
+    public function error($message = 'Error', $status = 400, $errors = [])
+    {
+        return response()->json(['success' => false, 'errors' => $errors, 'message' => $message], $status);
+    }
 }
